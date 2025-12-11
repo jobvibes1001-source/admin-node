d2dee030c00091213e9ae6b40bf19c1ff3293b81
Anil Rathore <anilrathore@Anils-MacBook-Pro-5.local>
2025-12-11 10:52:51 +0530

    initial code

--- src/utility/constant.js
+++ src/utility/constant.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv").config({ path: ".env" });
 
 const CONSTANT = {
   DB_URL: process.env.MONGO_URI,
-  NODE_PORT: process.env.NODE_PORT,
+  NODE_PORT: process.env.PORT || 3000,
   BASE_URL: process.env.BASE_URL,
 };
 
